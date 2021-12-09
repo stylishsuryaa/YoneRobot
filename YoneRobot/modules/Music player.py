2a96b5aee2c677a2f22cdd1c3e46870491eaabf7
stylishsuryaa <83962899+stylishsuryaa@users.noreply.github.com>
2021-12-09 06:42:51 +0530

    Create Music player.py

---
+++ YoneRobot/modules/Music player.py
@@ -0,0 +1,14 @@
+->Music•Player<-
+=>> *Song Playing* 🎧 
+❍ /play  - play song you requested
+❍ /song  - download songs you want quickly
+❍ /video  - download videos you want quickly
+=>> *Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+❍ /userbotjoin - invite assistant to your chat
+❍ /refresh - Refresh admin list
+
